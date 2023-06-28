b675e7cc094c84de57e478ef752895c476b4e365
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-06-28 00:55:37 -0400

    slurm script

---
+++ count_graph_sim.slurm.R
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Example of running R script with a job array
+
+#SBATCH -J count_graph_sim
+#SBATCH --partition=defq
+#SBATCH --array=1-100                    # how many tasks in the array
+#SBATCH --cpus-per-task=4                          # one CPU core per task
+#SBATCH -t 00:07:00
+#SBATCH -o try-%j-%a.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ssrikan2@jhu.edu
+
+# Load software
+ml anaconda
+conda activate r4-base
+
+# Run R script with a command line argument
+Rscript signal_simulation.R $SLURM_ARRAY_TASK_ID
