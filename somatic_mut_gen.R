f1ed1809f648bce92548fbf0aadb736b87bf71e6
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-07-07 19:40:58 -0400

    somatic mut scripts for three cell count graph

---
+++ somatic_mut_gen.R
@@ -0,0 +1,89 @@
+signal_func = function(x_vec) {
+  val = 1e-9 * ((1/6)* pmin(pmax(0, x_vec), 4) + (-4/15) * pmax(0, x_vec-4))
+  out_vec = -pmax(val, 0) + 2*10^(-9)/3
+  out_vec[x_vec > 6.5] = 0
+  out_vec
+}
+make_mut <- function() {
+  paste0(sample(c(letters, 1:9), size = 20, replace = T), collapse = "")
+}
+
+library(ape)
+library(qfm)
+library(tidyverse)
+library(igraph)
+library(ggraph)
+setwd('/home/ssrikan2/data-kreza1/smriti/qfm2')
+devtools::load_all()
+setwd('/home/ssrikan2/data-kreza1/smriti/MF_Signal_Simulation')
+job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+input_folder = 'three_cell_cg_ss5000'
+output_folder = 'three_cell_somatic_mut_ss5000'
+
+load(paste0('./',input_folder, '/count_graph_', job_id,'.rda'))
+
+tr = phylo_edges_to_tr(count_graph$phylo_edges)
+tr_dd = list_dd_and_tips_mod2(tr)$dd
+tr_node_time = count_graph$phylo_edges$length
+tr_node_start = count_graph$phylo_edges$in_time
+tr_node_end = count_graph$phylo_edges$out_time
+names(tr_node_time) = names(tr_node_start) = names(tr_node_end) = count_graph$phylo_edges$out_node
+
+tr_tips = list_dd_and_tips_mod2(tr)$tips
+
+# node_id = names(tr_tips)[[1]]
+cell_mut_tb = bind_rows(map(names(tr_tips), function(node_id) {
+  # constant rate
+  # Lambda = mut_rate * tr_node_time[[node_id]]
+  # signal
+  Lambda = integrate(signal_func, tr_node_start[node_id], tr_node_end[node_id])$value
+  n_mut = rbinom(1, 10^9, 1-exp(-Lambda))
+  # this version value is number of occurrences along the edge
+  # tibble(cell = tr_tips[[node_id]],
+  #        mut = make_mut(),
+  #        value = n_mut)
+  # this version generates one row for each mut that occurred
+  if (n_mut > 0) {
+    out_tb = expand.grid(node = node_id,
+                         cell = tr_tips[[node_id]],
+                         mut = map_chr(1:n_mut, function(i) {
+                           make_mut()
+                         }))
+  } else {
+    out_tb = NULL
+  }
+  out_tb
+  #return(NULL)
+}))
+cell_mut_tb$value = 1
+
+m <- spread(cell_mut_tb[2:4], mut, value, fill = 0)
+# m$type <- m$cell
+# m <- m[,c(1,16994,2:16993)]
+chr_mat = as.matrix(m[-1])
+rownames(chr_mat) = m[[1]]
+
+tr_cells_append = tr$tip.label[!tr$tip.label %in% rownames(chr_mat)]
+chr_mat_append = matrix(0, nrow = length(tr_cells_append), ncol = ncol(chr_mat))
+rownames(chr_mat_append) = tr_cells_append
+chr_mat = rbind(chr_mat, chr_mat_append)
+assertthat::assert_that(nrow(chr_mat) == length(tr$tip.label))
+
+mf_mat_cell_type = aveMatFac(chr_mat, get_type_from_id(rownames(chr_mat)))
+colnames(mf_mat_cell_type) = colnames(chr_mat)
+mf_mat_cell_type = as.data.frame(mf_mat_cell_type)
+mf_mat_cell_type$CellType = rownames(mf_mat_cell_type)
+
+
+mf_table = gather(mf_mat_cell_type, key = 'sequence', value = 'mosaic_fraction', - CellType)
+mf_to_time_tb = nest(mf_table, data = -c(sequence)) %>%
+  mutate(mf = map_dbl(data, function(data) {
+    mean(data$mosaic_fraction)
+  }))
+mf_to_time_tb = left_join(cell_mut_tb, mf_to_time_tb %>% select(mut = sequence, mf)) %>%
+  mutate(log2mf = log2(mf))
+mf_to_time_tb$node_time = ((count_graph$phylo_edges$out_time+count_graph$phylo_edges$in_time)[match(mf_to_time_tb$node, count_graph$phylo_edges$out_node)])/2
+mf_to_time_tb = select(mf_to_time_tb, c(log2mf, node_time))
+
+save(mf_table, mf_to_time_tb, file = paste0('./', output_folder, '/mf_tables_', job_id, '.rda'))
