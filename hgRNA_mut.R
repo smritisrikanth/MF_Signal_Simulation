1b211f0ad069e6ff23b674527db93b70ddde2d95
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2023-07-10 01:47:06 -0400

    hgRNA mut sims

---
+++ hgRNA_mut.R
@@ -0,0 +1,86 @@
+signal_func_hgRNA = function(x_vec) {
+  val = 5e-2 * ((1/6)* pmin(pmax(0, x_vec), 4) + (-4/15) * pmax(0, x_vec-4))
+  pmax(val, 0)
+}
+chrmat_to_onehot <- function(x, include_unmutated = F) {
+  onehot_list = lapply(1:ncol(x), function(i) {
+    y = x[, i]
+    y[is.na(y)] = "0"
+    if (all(y == "0")) {
+      return(NULL)
+    }
+    levels = sort(unique(y))
+    out_mat = 1 * do.call(cbind, lapply(levels, function(z) y ==
+                                          z))
+    colnames(out_mat) = paste0("site", i , "_", levels)
+    if (!include_unmutated) {
+      out_mat = out_mat[, colnames(out_mat) != paste0("site", i , "_0"), drop = F]
+    }
+    out_mat
+  })
+  out = do.call(cbind, onehot_list)
+  if (!is.null(out)) {
+    rownames(out) = rownames(x)
+  }
+  out
+}
+
+library(ape)
+library(qfm)
+library(tidyverse)
+library(igraph)
+library(ggraph)
+setwd('/home/ssrikan2/data-kreza1/smriti/qfm2')
+devtools::load_all()
+
+mut_p = readRDS("./metadata//mut_p_marc1.rds")
+mut_p$mut_rate_func = map(1:length(mut_p$mut_rate), function(i) {
+  signal_func_hgRNA
+})
+mut_p$mut_rate = NULL
+
+setwd('/home/ssrikan2/data-kreza1/smriti/MF_Signal_Simulation')
+job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
+
+input_folder = 'output'
+output_folder = 'one_cell_hgRNA_mut_ss5000'
+
+load(paste0('./',input_folder, '/count_graph_', job_id,'.rda'))
+
+chr_mat = simulate_all_phylogeny_bb_v1(count_graph, mut_p)
+chrmat_onehot = chrmat_to_onehot(x = chr_mat$chr_mat, include_unmutated = F)
+mut_frac_mat = aveMatFac(chrmat_onehot, fac = get_type_from_id(rownames(chrmat_onehot)))
+colnames(mut_frac_mat) = colnames(chrmat_onehot)
+Heatmap(mut_frac_mat[, 1:100])
+
+param_tb$mut_frac_mat[[i]] = mut_frac_mat
+
+mut_frac_mat = as.data.frame(mut_frac_mat)
+mut_frac_mat$CellType = rownames(mut_frac_mat)
+
+message(i)
+mut_frac_mat = gather(mut_frac_mat, key = 'sequence', value = 'mosaic_fraction', -CellType)
+
+mut_frac_mat$ID = as.numeric(substr(mut_frac_mat$sequence,
+                                    5,
+                                    as.numeric(gregexpr('R', mut_frac_mat$sequence))-2))
+mut_frac_mat$sequence = substr(mut_frac_mat$sequence,
+                               as.numeric(gregexpr('R', mut_frac_mat$sequence)),
+                               length(mut_frac_mat$sequence))
+
+
+anl = chr_mat$allele_node_list
+
+anl = map(anl, function(id) {
+  setNames(names(id), as.vector(id))
+})
+
+mf_to_time_tb = mut_frac_mat
+mf_to_time_tb$node = map2(mf_to_time_tb$ID, mf_to_time_tb$sequence, function(id, seq) {
+  anl[[id]][[seq]]
+})
+mf_to_time_tb = select(mf_to_time_tb, c(mosaic_fraction, node)) %>% mutate(log2mf = log2(mosaic_fraction))
+mf_to_time_tb$node_time = ((count_graph$phylo_edges$out_time+count_graph$phylo_edges$in_time)[match(mf_to_time_tb$node, count_graph$phylo_edges$out_node)])/2
+mf_to_time_tb = select(mf_to_time_tb, c(log2mf, node_time))
+
+save(mut_frac_mat, mf_to_time_tb, file = paste0('./', output_folder, '/mf_tables_', job_id, '.rda'))
