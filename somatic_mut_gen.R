3bd0a1e6bfbf7cd2042dfd6345c18e27b9fa103a
Smriti Srikanth <smritisrikanth@smritis-mbp.acasmart.jh.edu>
2023-08-07 11:26:15 -0400

    changed paths

--- somatic_mut_gen.R
+++ somatic_mut_gen.R
@@ -40,8 +40,8 @@ devtools::load_all()
 setwd('/home/ssrikan2/data-kreza1/smriti/MF_Signal_Simulation')
 job_id = as.numeric(Sys.getenv('SLURM_ARRAY_TASK_ID'))
 
-input_folder = 'three_cell_cg_ss5000_015_2_T_0.95_5'
-output_folder = 'three_cell_somatic_T_095_5_signal4'
+input_folder = 'output_ab0.15_ss1000'
+output_folder = 'one_cell_015_2_ss1000_somatic_signal4'
 
 
 load(paste0('./',input_folder, '/count_graph_', job_id,'.rda'))
