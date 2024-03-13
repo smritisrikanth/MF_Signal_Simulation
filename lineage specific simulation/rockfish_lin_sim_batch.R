78f0a21f5abbf5d273e83bdb823db0b8651410c4
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2024-03-13 15:29:55 -0400

    made batch script without array param

--- lineage specific simulation/rockfish_lin_sim_batch.R
+++ lineage specific simulation/rockfish_lin_sim_batch.R
@@ -3,7 +3,7 @@
 #SBATCH -J tree_reconstruction
 #SBATCH --partition=parallel               # how many tasks in the array
 #SBATCH --cpus-per-task=4                          # one CPU core per task
-#SBATCH -t 01:00:00
+#SBATCH -t 05:00:00
 #SBATCH -o a.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ssrikan2@jhu.edu
