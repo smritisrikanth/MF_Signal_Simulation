500f035592c9b4701dd221587363a32293b92cc3
Smriti Srikanth <smritisrikanth@Smritis-MacBook-Pro.local>
2024-04-02 23:19:20 -0400

    hgRNA scripts

---
+++ lineage specific simulation/hgRNA_batch.R
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J hgRNA_mut_lin_spec
+#SBATCH --partition=parallel               # how many tasks in the array
+#SBATCH --cpus-per-task=4                          # one CPU core per task
+#SBATCH -t 05:00:00
+#SBATCH -o a.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ssrikan2@jhu.edu
+
+# Load software
+ml anaconda
+conda activate r4-base
+
+# Run R script with a command line argument
+Rscript hgRNA_mutation_sim.R
